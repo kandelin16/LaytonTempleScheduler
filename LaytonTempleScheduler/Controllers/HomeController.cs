eb60f162b79aa1a4b7ee1fbb9037896dd84d8cc4
Kaden <kaden.souter@gmail.com>
2022-03-21 19:14:50 -0600

    Made an appointment form page and added an action in the controller for it

--- LaytonTempleScheduler/Controllers/HomeController.cs
+++ LaytonTempleScheduler/Controllers/HomeController.cs
@@ -42,5 +42,10 @@ namespace LaytonTempleScheduler.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Appointment()
+        {
+            return View();
+        }
     }
 }
