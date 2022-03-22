986ee32e239c030626752424a50750eeb4d59a98
Kaden <kaden.souter@gmail.com>
2022-03-21 20:11:41 -0600

    Added a confirmation page

--- LaytonTempleScheduler/Controllers/HomeController.cs
+++ LaytonTempleScheduler/Controllers/HomeController.cs
@@ -43,9 +43,16 @@ namespace LaytonTempleScheduler.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        [HttpGet]
         public IActionResult Appointment()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Appointment(Appointment a)
+        {
+            return View("Confirmation");
         }
     }
 }
